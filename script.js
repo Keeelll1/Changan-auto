0dbe6e9c1f8377f7fedea58fd60a06624b854e92
Artem <artem256korolev@gmail.com>
2023-11-27 11:47:22 +0300

    add dropdown

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ const btnOpen = document.querySelectorAll('.button'),
         popUp.classList.remove('active')
         overlay.classList.remove('active')
     })
+
+const downloadMore = document.querySelector('.donwload-more'),
+    addBlock = document.querySelector('.add-more-cards')
+
+    downloadMore.addEventListener('click', () => {
+        addBlock.style.display = 'block'
+        downloadMore.style.display = 'none'
+    })
+
+const dropBtn = document.querySelector('.all-model-button'),
+    dropContent = document.querySelectorAll('.model-menu-list')
+
+    dropBtn.addEventListener('click', () => {
+        dropBtn.classList.toggle('active')
+        dropContent.forEach(item => {
+            item.classList.toggle('active')
+        })
+    })
